0c7e630156b5746434ed8e3e08f43b66f75577a8
Acexy <acexy@acexy.cn>
2026-08-22 12:01:01 +0800

    Update portway beta to v0.1.4.beta

--- Formula/portway-beta.rb
+++ Formula/portway-beta.rb
@@ -1,31 +1,31 @@
 class PortwayBeta < Formula
   desc "Secure reverse tunneling client (beta)"
   homepage "https://github.com/acexy/portway"
-  version "0.1.3-rc"
+  version "0.1.4.beta"
   license "Apache-2.0"
   conflicts_with "portway", because: "both install the portway binary"
 
   on_macos do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.3-rc/portway-darwin-arm64.tar"
-      sha256 "618221e775e970b042bb546ce579d1ffbb4e4520ed9ee54d24ae4e20203443d7"
+      url "https://github.com/acexy/portway/releases/download/v0.1.4.beta/portway-darwin-arm64.tar"
+      sha256 "5c15c31581c5fb46125afa959af449650b382c6a197e9e5aef311e591d710ff6"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.3-rc/portway-darwin-amd64.tar"
-      sha256 "7017eb35a3b73ff863046749f5c88dd5561b0f6bfafed0aebf91319df461cc22"
+      url "https://github.com/acexy/portway/releases/download/v0.1.4.beta/portway-darwin-amd64.tar"
+      sha256 "5571fdcc8534b6db8d20773396035fc79e805437ee8673bb0b79cf4c599baefc"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.3-rc/portway-linux-arm64.tar"
-      sha256 "50663d8b87a8c3c3aabec2d132d4cbeba299b1001c0ab3a93f2d6f84ea18a961"
+      url "https://github.com/acexy/portway/releases/download/v0.1.4.beta/portway-linux-arm64.tar"
+      sha256 "5f5ca2088308ada67c2325de73b9d3a4f45b7893fdb5402d279fc7e9ff91130f"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.3-rc/portway-linux-amd64.tar"
-      sha256 "faa13bb94ad6ac4131d35473b34c2a49044d93ab979025a96868777f3e7c5cce"
+      url "https://github.com/acexy/portway/releases/download/v0.1.4.beta/portway-linux-amd64.tar"
+      sha256 "c24242e48b4880a79c7444dcb5271a9a86e77e9616dd87363daeff687a245e0d"
     end
   end
 

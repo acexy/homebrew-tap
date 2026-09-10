177c95652208d840b48c994553a82e943b64d9f9
Acexy <acexy@acexy.cn>
2026-09-10 16:26:00 +0800

    Update portway beta to v0.1.9.beta.2

--- Formula/portwayd-beta.rb
+++ Formula/portwayd-beta.rb
@@ -1,31 +1,31 @@
 class PortwaydBeta < Formula
   desc "Secure reverse tunneling server (beta)"
   homepage "https://github.com/acexy/portway"
-  version "0.1.9.beta"
+  version "0.1.9.beta.2"
   license "Apache-2.0"
   conflicts_with "portwayd", because: "both install the portwayd binary"
 
   on_macos do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9.beta/portway-darwin-arm64.tar"
-      sha256 "cf82ad92a6027c93868dd2bb111b829d4aba4838b8090ebe4351363069e4188f"
+      url "https://github.com/acexy/portway/releases/download/v0.1.9.beta.2/portway-darwin-arm64.tar"
+      sha256 "41095c28fab44f80cbc130861c47ab0afbffc037e9d553f44dfc974fc663c66e"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9.beta/portway-darwin-amd64.tar"
-      sha256 "785826254e67285079cda1232e6d71080af902d3626bb2fc413cdec34a401f14"
+      url "https://github.com/acexy/portway/releases/download/v0.1.9.beta.2/portway-darwin-amd64.tar"
+      sha256 "be065abef5999d2e81979bd77b6574cc0193ea94405d414c8d73e018f0261fdb"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9.beta/portway-linux-arm64.tar"
-      sha256 "47a20b566136fde6f71582be19feec195847b55c3698f266082be4700d88d498"
+      url "https://github.com/acexy/portway/releases/download/v0.1.9.beta.2/portway-linux-arm64.tar"
+      sha256 "72738566c30cb682c77ea08dc96b7c1f3f601281ec9711b7d0baa64d71755faf"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9.beta/portway-linux-amd64.tar"
-      sha256 "c67f37e70aa8764debb7992ec5af44bf6898934fe952c789cc3e7e4a97fd60ad"
+      url "https://github.com/acexy/portway/releases/download/v0.1.9.beta.2/portway-linux-amd64.tar"
+      sha256 "e720b9dc2f50ad6553dc3746a07d14c495ae500e61bc47227cee171a8dba6be1"
     end
   end
 

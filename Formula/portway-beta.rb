a3eea006dd78e6dc1d2346b1cccdedf16f6212dc
Acexy <acexy@acexy.cn>
2026-09-16 16:24:31 +0800

    Update Portway beta to v0.1.10.beta

--- Formula/portway-beta.rb
+++ Formula/portway-beta.rb
@@ -1,31 +1,31 @@
 class PortwayBeta < Formula
-  desc "Secure reverse tunneling client (beta)"
+  desc "Lightweight connectivity through Proxy, Forward, and VNet modes (beta)"
   homepage "https://github.com/acexy/portway"
-  version "0.1.9.RC"
+  version "0.1.10.beta"
   license "Apache-2.0"
   conflicts_with "portway", because: "both install the portway binary"
 
   on_macos do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9.RC/portway-darwin-arm64.tar"
-      sha256 "112ed5a5f16ce7231c043160e4910e22004b6419aaaf785614f960c1ae3af7bf"
+      url "https://github.com/acexy/portway/releases/download/v0.1.10.beta/portway-darwin-arm64.tar"
+      sha256 "7eb2a61f6c98cca80002b147e555d450e4dfd25647bd80d43c05f57aef1149c2"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9.RC/portway-darwin-amd64.tar"
-      sha256 "4ffa103ba2b4a474921e8af20edbfef2454e64c0574277488cd4b8c32d4c60e7"
+      url "https://github.com/acexy/portway/releases/download/v0.1.10.beta/portway-darwin-amd64.tar"
+      sha256 "aceb63621d5a886424a4211eec1f32bc9fdb307d4d8522369d80519994466c65"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9.RC/portway-linux-arm64.tar"
-      sha256 "2f3307bd9d35409325515ef6ae14d24d3ab9430a93b04caa5691b96676b6e20c"
+      url "https://github.com/acexy/portway/releases/download/v0.1.10.beta/portway-linux-arm64.tar"
+      sha256 "11157e3c5b358acb685207d7bd626bddb7a45a32c3d325cd7ba6d708543b08a7"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9.RC/portway-linux-amd64.tar"
-      sha256 "5a78c9dca0920847349dcc9aae072dde48aaa4679f6bb5626e49732d1e73a31a"
+      url "https://github.com/acexy/portway/releases/download/v0.1.10.beta/portway-linux-amd64.tar"
+      sha256 "cd85fb457784527b34543f296736301e96042e7b22bf32d2800e6e2cf9d5cb2c"
     end
   end
 
@@ -34,6 +34,8 @@ class PortwayBeta < Formula
   end
 
   test do
-    assert_match "version: v#{version}", shell_output("#{bin}/portway version")
+    output = shell_output("#{bin}/portway version")
+    assert_match "version: v#{version}", output
+    assert_match(/^core-protocol: \d+$/, output)
   end
 end

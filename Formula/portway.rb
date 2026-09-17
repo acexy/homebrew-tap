1c235c2f355042dff3f8fe851b9a4a820fa87f77
Acexy <acexy@acexy.cn>
2026-09-17 11:02:59 +0800

    Update Portway to v0.1.10

--- Formula/portway.rb
+++ Formula/portway.rb
@@ -1,28 +1,28 @@
 class Portway < Formula
-  desc "Secure reverse tunneling client"
+  desc "Lightweight connectivity through Proxy, Forward, and VNet modes"
   homepage "https://github.com/acexy/portway"
   license "Apache-2.0"
   on_macos do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9/portway-darwin-arm64.tar"
-      sha256 "35515e8f72f42bb4ff6c5afe7e9b94ee14cdf9afbb011eea5dd6578eae5d8286"
+      url "https://github.com/acexy/portway/releases/download/v0.1.10/portway-darwin-arm64.tar"
+      sha256 "73f2b402b00b21f2abb81da67859d12fafeaaef8070fb8d4e48724842038ecff"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9/portway-darwin-amd64.tar"
-      sha256 "3157954e7e1ae8ec22df503fd02cd715e53516ca901ab231011b0f23265b9c1d"
+      url "https://github.com/acexy/portway/releases/download/v0.1.10/portway-darwin-amd64.tar"
+      sha256 "fd3d22b5c1c5341338d12cbb5fb0fca7d34d3ce28d933059c47b05ace3228306"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9/portway-linux-arm64.tar"
-      sha256 "9305c602e163bccd50180d5db9adc477686056997e67cfe45cb0d60045b59ff9"
+      url "https://github.com/acexy/portway/releases/download/v0.1.10/portway-linux-arm64.tar"
+      sha256 "c1aa3906f11dfe099ec9f0b039cc7c199d01b83da1474cc7e9a98029a2e47a48"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.9/portway-linux-amd64.tar"
-      sha256 "572785963fba0fddfd6dca6568d9a1ac2dbd562aaf0952ef829d17d6eb4ba9a5"
+      url "https://github.com/acexy/portway/releases/download/v0.1.10/portway-linux-amd64.tar"
+      sha256 "97a70c11f67b9fc679e1f28dc30e399762805d4f0e58d6a525e566609103d8b5"
     end
   end
 

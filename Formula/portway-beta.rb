f2fb5b4a4f1819927eb4823041813dc926f41f9f
Acexy <acexy@acexy.cn>
2026-09-20 15:35:54 +0800

    Update portway beta to v0.1.11.beta.2

--- Formula/portway-beta.rb
+++ Formula/portway-beta.rb
@@ -1,31 +1,31 @@
 class PortwayBeta < Formula
   desc "Lightweight connectivity through Proxy, Forward, and VNet modes (beta)"
   homepage "https://github.com/acexy/portway"
-  version "0.1.11.beta"
+  version "0.1.11.beta.2"
   license "Apache-2.0"
   conflicts_with "portway", because: "both install the portway binary"
 
   on_macos do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta/portway-darwin-arm64.tar"
-      sha256 "6cd7a0aef9d169676558406f9b93e12508f63d9cf6013bd2107b92e1ed46125b"
+      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.2/portway-darwin-arm64.tar"
+      sha256 "ed1b9475808a7774eae4da2fac2b713cc2c2326fe1ac27957d4ab7094aeaf1ff"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta/portway-darwin-amd64.tar"
-      sha256 "50ea2fd767b8adc5a6d2d51f30faa90068eca03929c365e467db61359ac2be3d"
+      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.2/portway-darwin-amd64.tar"
+      sha256 "688047bca083b0e73a9236ac1cf9db0840579d5b3514d69d0d62dd663e5577f1"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta/portway-linux-arm64.tar"
-      sha256 "2fa4db9ba445c67ba3bf767152cb5ad1648948e57bed323813fab1e22a8fbb21"
+      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.2/portway-linux-arm64.tar"
+      sha256 "3f72ccf4f2b8013004195a89cbc37604a31c4ce1bfa036270735b20be655035b"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta/portway-linux-amd64.tar"
-      sha256 "99154fe0af7b3811208c5e0a17f5eb8e5c18786938e409e2521afce24e4fae49"
+      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.2/portway-linux-amd64.tar"
+      sha256 "3995a9e07dbe6fa94840740165f0fc18f326090ebdf62c696e1f28495d327bf4"
     end
   end
 

f032943bcdb404f9dc89c41715052ccbac98995d
Acexy <acexy@acexy.cn>
2026-09-21 09:36:38 +0800

    Update portway beta to v0.1.11.beta.3

--- Formula/portway-beta.rb
+++ Formula/portway-beta.rb
@@ -1,31 +1,31 @@
 class PortwayBeta < Formula
   desc "Lightweight connectivity through Proxy, Forward, and VNet modes (beta)"
   homepage "https://github.com/acexy/portway"
-  version "0.1.11.beta.2"
+  version "0.1.11.beta.3"
   license "Apache-2.0"
   conflicts_with "portway", because: "both install the portway binary"
 
   on_macos do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.2/portway-darwin-arm64.tar"
-      sha256 "ed1b9475808a7774eae4da2fac2b713cc2c2326fe1ac27957d4ab7094aeaf1ff"
+      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.3/portway-darwin-arm64.tar"
+      sha256 "fafddfcbc4460b782829c9b820b21a1f451ab8506b478e7b850fbd3544c5693c"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.2/portway-darwin-amd64.tar"
-      sha256 "688047bca083b0e73a9236ac1cf9db0840579d5b3514d69d0d62dd663e5577f1"
+      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.3/portway-darwin-amd64.tar"
+      sha256 "2dbebbd4b3762564cb397b4d43ea174fd2310e5e18ea196c6c69391c92bf4032"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.2/portway-linux-arm64.tar"
-      sha256 "3f72ccf4f2b8013004195a89cbc37604a31c4ce1bfa036270735b20be655035b"
+      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.3/portway-linux-arm64.tar"
+      sha256 "869215dce3007dcbf509f93abd40f089f49f10c74f7af464e589e77cf4179582"
     end
 
     on_intel do
-      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.2/portway-linux-amd64.tar"
-      sha256 "3995a9e07dbe6fa94840740165f0fc18f326090ebdf62c696e1f28495d327bf4"
+      url "https://github.com/acexy/portway/releases/download/v0.1.11.beta.3/portway-linux-amd64.tar"
+      sha256 "32947d19ab7f60fe7e11adf0a10511fa41b988bf50c221d4843415f39315d4a2"
     end
   end
 
